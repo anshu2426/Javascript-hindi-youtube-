7add6688d7a6666fbdd02c2a970d0a7090ad7897
Anshul yadav <163381544+anshu2426@users.noreply.github.com>
2025-11-12 14:05:43 +0530

    Add variable and datatype JS files
    Uploaded 2variable.js and 3datatype.js for JavaScript Hindi tutorial practice.

---
+++ 2variable.js
@@ -0,0 +1,12 @@
+const accountId=123456;
+let accountName="Rahul";
+var accountPin=134;
+accountCity="Delhi";//we can use this without deleclaring because it is global variable
+// accountId=123; we can't change const variable
+accountName="anshul" //we can change let variable
+accountPin=1234 //we can change var variable
+console.log(accountId);
+console.log(accountName);
+console.log(accountPin);
+console.log(accountCity);
+//we don't use var because of its scope
---
+++ 3datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+// alert("hello"); this thing doesn't work on vs code as it is only use in browser
+let name = "hitesh";
+let age = 12;
+let isloggenin = true;
+let state = null;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isloggenin);
+console.log(typeof state);
